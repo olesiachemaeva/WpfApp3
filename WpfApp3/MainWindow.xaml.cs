ee86c3af430e3fd1b0734764287460cf94dbc82c
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-02-14 00:10:32 +1000

    Задача 9

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -151,6 +151,14 @@ namespace WpfApp3
                 e.CanExecute = false;
             }
         }
+
+        private void themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Uri theme = new Uri(themes.SelectedIndex == 0 ? "Light.xaml" : "Dark.xaml", UriKind.Relative);
+            ResourceDictionary themeDict = Application.LoadComponent(theme) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
     }
     
 }
